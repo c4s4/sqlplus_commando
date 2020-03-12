5ff22666910448569c697d1d5d645419030ad765
SLASH_CyberPunk <slash.cyberpunk@gmail.com>
2020-03-12 17:15:59 +0300

    check python version

--- sqlplus_commando/__init__.py
+++ sqlplus_commando/__init__.py
@@ -2,4 +2,8 @@
 # encoding: UTF-8
 
 #pylint: disable=W0403
-from sqlplus_commando import SqlplusCommando, SqlplusResultParser, SqlplusErrorParser
+import sys
+if sys.version_info.major == 2:
+    from sqlplus_commando import SqlplusCommando, SqlplusResultParser, SqlplusErrorParser
+elif sys.version_info.major == 3:
+    from .sqlplus_commando import SqlplusCommando, SqlplusResultParser, SqlplusErrorParser

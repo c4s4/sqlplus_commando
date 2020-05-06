e113d0dca310ae1da1db347aa8cc7ba80c712ce0
Michel Casabianca <casa@sweetohm.net>
2020-05-06 12:07:53 +0200

    Project refactoring and port to Python 3

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: UTF-8
+
+from distutils.core import setup
+
+setup(
+    name = 'sqlplus_commando',
+    version = '0.0.0',
+    author = 'Michel Casabianca',
+    author_email = 'casa@sweetohm.net',
+    packages = ['sqlplus_commando'],
+    url = 'http://pypi.python.org/pypi/sqlplus_commando/',
+    license = 'Apache Software License',
+    description = 'sqlplus_commando is an Oracle driver calling sqlplus',
+    long_description=open('README.rst', encoding='UTF-8').read(),
+)
